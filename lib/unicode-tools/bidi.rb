d7a52a205834bf5159fa1ad7d2323c343bd2effa
Yaroslav Konoplov <yaroslav@inbox.com>
2016-01-18 18:21:49 +0200

    Add test methods

--- lib/unicode-tools/bidi.rb
+++ lib/unicode-tools/bidi.rb
@@ -2,6 +2,10 @@ module UnicodeTools
   BIDI_OVERRIDE_CHARS_REGEX = /\u00E2\u0080[\u008E\u008F\u00AA-\u00AE]/
 
   class << self
+    def has_bidi_override?(string)
+      !!(string =~ BIDI_OVERRIDE_CHARS_REGEX)
+    end
+
     def strip_bidi_override_chars(string)
       string.gsub(BIDI_OVERRIDE_CHARS_REGEX, '')
     end
