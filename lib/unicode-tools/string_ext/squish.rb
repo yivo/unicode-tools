6bead4984e852232acaf42c3ee19d6b2aea0289c
Yaroslav Konoplov <yaroslav@inbox.com>
2016-06-01 13:45:00 +0300

    Fix comment

--- lib/unicode-tools/string_ext/squish.rb
+++ lib/unicode-tools/string_ext/squish.rb
@@ -6,7 +6,7 @@ module UnicodeTools
       #           \u2001\u2002​\u2003\u2004​\u2005\u2006
       #           ​\u2007\u2008​\u2009\u200a​\u2028\u2029​
       #           \u2028\u2029​\u202f\u205f​\u3000 World!"
-      # string.squish => "HelloWorld!"
+      # string.squish => "Hello World!"
       def squish
         gsub(WHITESPACE_REGEX, ' ')
       end
