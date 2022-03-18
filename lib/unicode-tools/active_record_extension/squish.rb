2f2822f62c22b2778c151e2f84c66fa9c223e0ea
Yaroslav Konoplov <eahome00@gmail.com>
2022-03-18 17:39:21 +0200

    Nullify string if it's blank in ActiveRecord extension

--- lib/unicode-tools/active_record_extension/squish.rb
+++ lib/unicode-tools/active_record_extension/squish.rb
@@ -10,7 +10,7 @@ module UnicodeTools
         attributes.each do |attribute|
           extension.class_eval <<-RUBY, __FILE__, __LINE__ + 1
             def #{attribute}=(value)
-              super(String === value ? value.squish : value)
+              super(String === value ? value.squish.presence : value)
             end
           RUBY
         end
